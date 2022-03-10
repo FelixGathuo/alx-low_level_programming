33fd6783929664c38214c983f97c89a6bd8523d7
Felix Gathuo <gathuofelix@gmail.com>
2022-03-10 13:28:56 +0000

    Size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,11 @@ char a;
 int b;
 long int c;
 long long int d;
-printf("Size of a char: ", sizeof(a));
-printf("Size of an int: ", sizeof(b));
-printf("Size of a long int: ", sizeof(c));
-printf("Size of a long long int: ", sizeof(d));
+float e;
+printf("Size of a char: %lu byte(s)\n", sizeof(a));
+printf("Size of an int: %lu byte(s)\n", sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+printf("Size of a float: %lu byte(s)\n", sizeof(e));
 return (0);
 }
