04207397e73f0a4575109b6678739c9b6c9215e2
Felix Gathuo <gathuofelix@gmail.com>
2022-03-10 14:05:31 +0000

    print to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  *main - Entry point
@@ -8,6 +9,6 @@
 
 int main(void)
 {
-fwrite("and that piece of art is useful\" - Dora Korpar, '2015-10-19'\n");
+fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (0);
 }
