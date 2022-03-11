68b53ba871d3e19010de4e27c45116a8d7d0374f
Felix Gathuo <gathuofelix@gmail.com>
2022-03-11 14:20:33 +0000

    alphabets lower and upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+i = 97;
+j = 65;
+while (i < 123)
+{
+putchar(i);
+i++;
+}
+while (j < 91)
+{
+putchar(j);
+j++
+}
+putchar('\n');
+return (0);
+}
