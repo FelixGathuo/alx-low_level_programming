043984e6832fb9887400028ec1c095033bd44adf
Felix Gathuo <gathuofelix@gmail.com>
2022-03-11 14:50:08 +0000

    numbers base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *
+ * main print numbers 0-9
+ *
+ * Return: (0) Success
+ */
+int main(void)
+{
+int x;
+for (x = 0; x < 10, x++)
+printf(x);
+printf('\n');
+return (0);
+}
