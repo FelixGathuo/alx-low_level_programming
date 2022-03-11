a570e99601a1557bb93863104ea636106cc8e551
Felix Gathuo <gathuofelix@gmail.com>
2022-03-11 14:37:40 +0000

    alphabets lower minus q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ *  *main - Entry point
+ *   *
+ *    *Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+int i;
+i = 97;
+while (i < 123)
+{
+if !((i == 69 || i == 81)
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
