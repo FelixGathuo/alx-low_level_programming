6fc342e780eeea307012da5e6dc3b2f5c3909e80
Felix Gathuo <gathuofelix@gmail.com>
2022-03-11 15:49:34 +0000

    numbers base 10

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+i = 123;
+while (i > 96)
+{
+putchar(i);
+i--;
+}
+putchar('\n');
+return (0);
+}
