b59817b4c96dcabf8a6c873d1023929cde279ff7
Felix Gathuo <gathuofelix@gmail.com>
2022-03-11 16:00:11 +0000

    number base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+char j;
+j = 'a'
+i = 10;
+while (i < 10)
+{
+putchar(i + '0');
+i++;
+}
+while (j < 'f')
+{
+putchar(j);
+j++;
+}
+putchar('\n');
+return (0);
+}
