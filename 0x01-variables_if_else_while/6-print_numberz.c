69a82380353aca6e6f7827f8461a8c27e7f36cbc
Felix Gathuo <gathuofelix@gmail.com>
2022-03-11 15:21:50 +0000

    numbers base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+i = 10;
+while (i < 10)
+{
+putchar("%d", i);
+i++;
+}
+putchar('\n');
+return (0);
+}
