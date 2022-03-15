d87d05012a97c26eaac9b321a4b5b84e4e2afaa0
Felix Gathuo <gathuofelix@gmail.com>
2022-03-15 19:58:42 +0000

    sum of ints

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result.
+ * @a, @b are two integers
+ * 
+ * Return: no return
+ */
+int add(int a, int b)
+{
+int a;
+int b;
+res;
+res = a+b;
+return(res);
+}
