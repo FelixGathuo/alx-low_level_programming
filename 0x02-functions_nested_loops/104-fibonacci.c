4f459e17154bb03eb405a0faf122d01d6bc2e188
FelixGathuo <40053042+FelixGathuo@users.noreply.github.com>
2022-03-16 00:23:34 +0300

    first 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 
 /**
-* main - Prints first 50 fibonnaci numbers
+* main - Prints first 98 fibonnaci numbers
 *
 * Return: Always 0.
 */
 int main(void)
 {
 long int a = 1, b = 2, c, count = 0;
-while (count < 97)
+while (count < 98)
 {
 printf("%lu", a);
 c = a + b;
