4798a2fe6a695576195edf53bc751667ff50718a
FelixGathuo <40053042+FelixGathuo@users.noreply.github.com>
2022-03-16 00:17:14 +0300

    first 98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+* main - Prints first 50 fibonnaci numbers
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+long int a = 1, b = 2, c, count = 0;
+while (count < 97)
+{
+printf("%lu", a);
+c = a + b;
+a = b;
+b = c;
+printf("%c%c", ',', ' ');
+count++;
+}
+printf("%lu", a);
+printf("\n");
+return (0);
+}
