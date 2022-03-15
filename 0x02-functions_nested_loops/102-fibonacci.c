17f718246a7beae63ed0058d3865cdcac2e41865
FelixGathuo <40053042+FelixGathuo@users.noreply.github.com>
2022-03-15 23:48:45 +0300

    fibo

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,11 +10,13 @@ int main()
     int a = 1, b = 2, c, count = 0;
    while (count < 50)
    {
-     printf("%d%c\n", a, ',');
+     printf("%d", a);
      c = a + b;
      a = b;
      b = c;
      count++;
+     printf("%c", ',');
    }
+printf('\n');
 return (0);
 }
