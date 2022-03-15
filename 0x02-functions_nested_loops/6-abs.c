939f091d5be9d5e1d40915f5039432c4f92c0cd4
Felix Gathuo <gathuofelix@gmail.com>
2022-03-15 19:16:16 +0000

    sign

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_sign - prints sign of a number
+ *@n: The input number
+ * Return: Always 0
+ */
+int _abs(int)
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+return (n * -1);
+}
+}
