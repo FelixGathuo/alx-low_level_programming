198a542dcbe7bea897f7bf404e875740f6a47a64
Felix Gathuo <gathuofelix@gmail.com>
2022-03-15 18:59:35 +0000

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - prints sign of a number
+ *@c: The character in ASCII code
+ * Return: Always 0
+ */
+int print_sign(int n)
+{
+if (n >= 1)
+{
+return (1);
+_putchar('+')
+}
+else if (n == 0)
+{
+return (0);
+_putchar('0')
+}
+else
+{
+return (-1);
+_putchar('-')
+}
+_putchar('\n');
+}
