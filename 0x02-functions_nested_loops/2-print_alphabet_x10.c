0b5b58d186e3bce58ce51bfa1197f9416db0e380
Felix Gathuo <gathuofelix@gmail.com>
2022-03-15 18:36:23 +0000

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,13 @@
  */
 void print_alphabet_x10(void);
 {
-int i, a;
-a = 0;
-while (a < 11)
+int i, j;
+for (j = 0; j < 10)
 {
 for (i = 97; i < 123; i++)
 {
 _putchar(i);
 }
-a++;
-}
 _putchar('\n');
+}
 }
