e2fc97acf2f4e28f850a91238566749abe1382ba
FelixGathuo <40053042+FelixGathuo@users.noreply.github.com>
2022-03-16 00:12:06 +0300

    sum even

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+* main - Prints sum of even fibonnaci numbers
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+long int a = 1, b = 2, c, sum = 2;
+while (a < 4000000)
+{
+c = a + b;
+if (c%2 == 0)
+{
+sum += c;
+}
+a = b;
+b = c;
+}
+return (0);
+}
