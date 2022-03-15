fb8fa5369c10906a19a8293ad7cd627b494c2fe7
Felix Gathuo <gathuofelix@gmail.com>
2022-03-15 18:26:08 +0000

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print the alphabet in lowercase.
+ *
+ * Return: void.
+ */
+void print_alphabet_x10(void);
+{
+int i;
+int a = 0;
+while (a < 11)
+}
+for (i = 97; i < 123; i++)
+{
+_putchar(i);
+}
+a++;
+}
+_putchar('\n');
+}
