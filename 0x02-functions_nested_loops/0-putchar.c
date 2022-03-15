6b19622d03921d7681322d561aca512bad0d16ce
Felix Gathuo <gathuofelix@gmail.com>
2022-03-15 18:13:39 +0000

    print alphabet

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  *
@@ -6,7 +5,15 @@
  *Return: Always 0 (Success)
  */
 
-int _putchar(char c)
+int main(void)
 {
-return (write(1, &c, 1));
+int i;
+char a[] = "_putchar";
+
+for (i = 0; i < 9; i++)
+}
+_putchar(p[i]);
+}
+_putchar('\n');
+return (0);
 }
