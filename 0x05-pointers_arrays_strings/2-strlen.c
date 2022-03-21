5070b0c8cf09f5dcad7f041a2b73b3fcd44a3cb6
Felix Gathuo <gathuofelix@gmail.com>
2022-03-21 09:54:23 +0000

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main/h"
+/**
+* _strlen - returns len of string
+* @s: character
+*/
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
