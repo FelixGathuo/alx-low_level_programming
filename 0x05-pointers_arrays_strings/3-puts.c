2f9870200cb548d7987007b2eeca87e842861b89
Felix Gathuo <gathuofelix@gmail.com>
2022-03-21 10:01:14 +0000

    prints in stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <unistd.h>
+/**
+* _puts - prints in stdout
+* @str: character
+*Return: none
+*/
+void _puts(char *str)
+{
+	int l = 0;
+	while (*(str + l) != '\0')
+	{
+		_putchar(str[l]);
+		l++;
+	}
+	_putchar('\n');
+}
