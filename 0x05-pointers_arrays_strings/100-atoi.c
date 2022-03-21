c2bf3fad9a29814b069b3dde9b5cfcc215e4f37c
FelixGathuo <40053042+FelixGathuo@users.noreply.github.com>
2022-03-21 14:36:58 +0300

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * _atoi - converts a string to an integer.
- * @s: the string to convert
- * Return: the converted string.
- */
+* _atoi - converts a string to an integer.
+* @s: the string to convert
+* Return: the converted string.
+*/
 int _atoi(char *s)
 {
 	short boolean;
 	int i, minus, result;
-
+	
 	i = minus = result = boolean = 0;
 	minus = -1;
-
+	
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
