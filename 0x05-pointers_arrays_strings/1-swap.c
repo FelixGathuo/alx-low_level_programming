73ed7f2277042a822599dac9f50eb87b1273985d
Felix Gathuo <gathuofelix@gmail.com>
2022-03-21 09:46:59 +0000

    swap ints

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* swap_int - swap values of two ints
+* @a: integer
+* @b: integer
+* Return: not
+*/
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = n;
+}
