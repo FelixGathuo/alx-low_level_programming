a96e5ca8aa84130646ad70edf548c23ae39318fc
Felix Gathuo <gathuofelix@gmail.com>
2022-03-23 16:46:47 +0000

    string appending

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+nclude "main.h"
+/**
+*_strcat - concatenates two strings
+* @dest: input value
+* @src: input value
+*
+*Return: dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i=0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
