667e4d9ca275e603947cda811832be313b21c6de
Felix Gathuo <gathuofelix@gmail.com>
2022-03-23 17:14:16 +0000

    string appending

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
-*_strcat - concatenates two strings
-*@dest: input value
-*@src: input value
+*_strncat - string concat
+*@dest: input string
+*@src: input string
+*@n: input integer
 *Return: dest
 */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
-	i = 0;
 
+	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
 	j = 0;
-
-	while (src[j] != '\0')
+	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
