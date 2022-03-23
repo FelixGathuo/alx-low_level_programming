f327e5bcd2bd9b3406f838cd62bd2439749ba60f
Felix Gathuo <gathuofelix@gmail.com>
2022-03-23 17:42:24 +0000

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*reverse_array - reverse array
+*@a: input integer
+*@n: integer
+*Return: 0
+*/
+void reverse_array(int *a, int n)
+{
+	int *rev_arr;
+	n = n-1;
+
+	for (; n >= 0; n--)
+	{
+		rev_arr[j] = a[n];
+		j++;
+	}
+	return (rev_arr)
+}
