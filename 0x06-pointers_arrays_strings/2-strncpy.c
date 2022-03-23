51257e1b9aa879c1f60bd9d0286040b356426cf0
Felix Gathuo <gathuofelix@gmail.com>
2022-03-23 17:24:25 +0000

    string appending

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*_strncpy - copy string content
+*@dest: input value
+*@src: input value
+*@n: input value
+*Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
